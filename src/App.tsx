ba795a2a4de07bd8b8512f8e0d810eb3d1fdf9a3
Set Israel Ortiz Monasterio <set.israel@globalsysinfo.com>
2023-06-22 22:13:11 +0200

    load data using the api key

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+const TMDB_BASE_URL = process.env.REACT_APP_TMDB_BASE_URL;
 
 function App() {
+
+  useEffect(() => {
+    const url = `${TMDB_BASE_URL}movie/11?api_key=${API_KEY}`;
+    axios.get(url).then(res => console.log('Res ', res.data));
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
